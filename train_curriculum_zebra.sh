dd0542e05aeb2872039813723160824f22aba5c3
wangboyu-langya <hxianglong@gmail.com>
2025-08-10 07:33:05 -0700

    fix typo

--- train_curriculum_zebra.sh
+++ train_curriculum_zebra.sh
@@ -7,8 +7,7 @@ MODEL_PATH=Qwen/Qwen2.5-7B-Instruct-1M
 export VLLM_ATTENTION_BACKEND=XFORMERS
 EPOCHS_PER_LEVEL=5
 BASE_DATA_DIR="data/loong/zebra_raw_levels"
-BASE_CHECKPOINT_DIR="/dss/dssmcmlfs01/pn39qo/pn39qo-dss-0000/di35qir2/camel
-/loong_checkpoints/zebra_curriculum"
+BASE_CHECKPOINT_DIR="/dss/dssmcmlfs01/pn39qo/pn39qo-dss-0000/di35qir2/camel/loong_checkpoints/zebra_curriculum"
 BASE_LOG_DIR="./logs/zebra_curriculum"
 
 # Create directories
